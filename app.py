c4a909b979f67527f4566a44443ead14c7238569
belluki <belluk.kim@gmail.com>
2024-04-08 22:17:05 +0900

    SiteGPT #2

--- app.py
+++ app.py
@@ -46,6 +46,46 @@ answers_prompt = ChatPromptTemplate.from_template("""
 """)
 
 
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+
+st.title("SiteGPT")
+st.sidebar.markdown("""
+[💻Github repo &rarr;](https://github.com/bellukki/fullstack-gpt)  
+[📜Code of app &rarr;](https://github.com/bellukki/fullstack-gpt/blob/SiteGPT/app.py)
+        """)
+
+url_cloudflare = "https://developers.cloudflare.com/sitemap.xml"
+url_openAI = "https://openai.com/sitemap.xml"
+
+api_key = st.sidebar.text_input(
+    "Put your OpenAI API Key here", type="password")
+
+if api_key:
+    llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
+        temperature=0.1,
+        api_key=api_key,
+        callbacks=[StreamingStdOutCallbackHandler()],
+    )
+    st.markdown(
+        """
+        Ask questions about the content of a website.
+                
+        Start by writing the URL of the website on the sidebar.
+
+        Or select the SITE what you want to find.
+
+        If you are forced to disconnect, please wait 3 seconds and select again.
+    """
+    )
+else:
+    st.warning("Please enter your OpenAI API Key first!!")
+
+
 def get_answers(inputs):
     docs = inputs['docs']
     question = inputs['question']
@@ -112,20 +152,8 @@ def parse_page(soup):
     )
 
 
-def parse_cfpage(soup, keywords):
-    text = (
-        str(soup.get_text())
-        .replace("\n", " ")
-        .replace("\xa0", " ")
-        .replace("closeSearch Submit Blog", "")
-    )
-    if any(keyword in text for keyword in keywords):
-        return text
-    return None
-
-
 @st.cache_resource(show_spinner="Loading Website...")
-def load_website(url):
+def load_website(url, api_key):
     splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1000,
         chunk_overlap=200
@@ -141,114 +169,79 @@ def load_website(url):
 
 
 @st.cache_resource(show_spinner="Loading Website...")
-def load_cfwebsite(url, keywords):
+def load_cfwebsite(url, api_key):
     splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1000,
         chunk_overlap=200
     )
     loader = SitemapLoader(
         url,
-        parsing_function=lambda soup: parse_cfpage(soup, keywords)
+        filter_urls=[
+            r"^(.*\/workers-ai\/).*",
+            r"^(.*\/vectorize\/).*",
+            r"^(.*\/ai-gateway\/).*",
+        ],
+        parsing_function=parse_page
     )
     loader.requests_per_second = 2
     docs = loader.load_and_split(text_splitter=splitter)
-    filtered_docs = [doc for doc in docs if doc is not None]
-    vector_store = FAISS.from_documents(filtered_docs, OpenAIEmbeddings())
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
     return vector_store.as_retriever()
 
 
-st.set_page_config(
-    page_title="SiteGPT",
-    page_icon="🖥️",
-)
-
-
-st.title("SiteGPT")
-st.sidebar.markdown("""
-[💻Github repo &rarr;](https://github.com/bellukki/fullstack-gpt)  
-[📜Code of app &rarr;](https://github.com/bellukki/fullstack-gpt/blob/QuizGPT-Turbo/app.py)
-        """)
-
-url_cloudflare = "https://developers.cloudflare.com/sitemap.xml"
-url_openAI = "https://openai.com/sitemap.xml"
-keywords = ["AI Gateway", "Cloudflare Vectorize", "Workers AI"]
-
-api_key = st.sidebar.text_input(
-    "Put your OpenAI API Key here", type="password")
-
-if api_key:
-    llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=0.1,
-        api_key=api_key,
-        callbacks=[StreamingStdOutCallbackHandler()],
+with st.sidebar:
+    choice = st.selectbox(
+        "Choose what you want to find documentation.",
+        (
+            "Search for what you want",
+            "Cloudflare",
+            "OpenAI",
+        ),
     )
-    st.markdown(
-        """
-        Ask questions about the content of a website.
-                
-        Start by writing the URL of the website on the sidebar.
-
-        Or select the SITE what you want to find.
-
-        If you are forced to disconnect, please wait 3 seconds and select again.
-    """
-    )
-    with st.sidebar:
-        choice = st.selectbox(
-            "Choose what you want to find documentation.",
-            (
-                "Search for what you want",
-                "Cloudflare",
-                "OpenAI",
-            ),
+    if choice == "Cloudflare":
+        url = url_cloudflare
+    elif choice == "OpenAI":
+        url = url_openAI
+    else:
+        url = st.text_input(
+            "Write down a URL",
+            placeholder="https://example.com/sitemap.xml",
         )
-        if choice == "Cloudflare":
-            url = url_cloudflare
-        elif choice == "OpenAI":
-            url = url_openAI
-        else:
-            url = st.text_input(
-                "Write down a URL",
-                placeholder="https://example.com/sitemap.xml",
+if url:
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Plaese write down a Sitemap URL")
+    elif url == url_cloudflare:
+        retriever = load_cfwebsite(url, api_key)
+        query = st.text_input(
+            """
+            Ask a question to the Cloudflare's each one of these products:
+            - AI Gateway
+            - Cloudflare Vectorize
+            - Workers AI
+            """)
+        if query:
+            chain = (
+                {
+                    "docs": retriever,
+                    "question": RunnablePassthrough(),
+                }
+                | RunnableLambda(get_answers)
+                | RunnableLambda(choose_answer)
             )
-    if url:
-        if ".xml" not in url:
-            with st.sidebar:
-                st.error("Plaese write down a Sitemap URL")
-        elif url == url_cloudflare:
-            retriever = load_cfwebsite(url, keywords)
-            query = st.text_input(
-                """
-                Ask a question to the Cloudflare's each one of these products:
-                - AI Gateway
-                - Cloudflare Vectorize
-                - Workers AI
-                """)
-            if query:
-                chain = (
-                    {
-                        "docs": retriever,
-                        "question": RunnablePassthrough(),
-                    }
-                    | RunnableLambda(get_answers)
-                    | RunnableLambda(choose_answer)
-                )
-                result = chain.invoke(query)
-                st.markdown(result.content.replace("$", "\$"))
-        else:
-            retriever = load_website(url)
-            query = st.text_input("Ask a question to the website.")
-            if query:
-                chain = (
-                    {
-                        "docs": retriever,
-                        "question": RunnablePassthrough(),
-                    }
-                    | RunnableLambda(get_answers)
-                    | RunnableLambda(choose_answer)
-                )
-                result = chain.invoke(query)
-                st.markdown(result.content.replace("$", "\$"))
-else:
-    st.warning("Please enter your OpenAI API Key first!!")
+            result = chain.invoke(query)
+            st.markdown(result.content.replace("$", "\$"))
+    else:
+        retriever = load_website(url, api_key)
+        query = st.text_input("Ask a question to the website.")
+        if query:
+            chain = (
+                {
+                    "docs": retriever,
+                    "question": RunnablePassthrough(),
+                }
+                | RunnableLambda(get_answers)
+                | RunnableLambda(choose_answer)
+            )
+            result = chain.invoke(query)
+            st.markdown(result.content.replace("$", "\$"))
