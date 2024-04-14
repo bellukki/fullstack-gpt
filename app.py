fabc0ae873c2fd8ba058871351a59930b0c5ce65
belluki <belluk.kim@gmail.com>
2024-04-14 20:41:18 +0900

    Final #1

--- app.py
+++ app.py
@@ -1,241 +1,309 @@
-# import asyncio
-# import sys
-import os
-import hashlib
-from langchain_community.document_loaders import SitemapLoader
-from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
-from langchain_openai import OpenAIEmbeddings
-from langchain.embeddings import CacheBackedEmbeddings
-from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
 import streamlit as st
-from streamlit.runtime.legacy_caching import clear_cache
-from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.storage import LocalFileStore
+import requests
+from bs4 import BeautifulSoup
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_community.tools import DuckDuckGoSearchResults
+import openai as client
+from dotenv import load_dotenv
+import time
+import json
+import datetime
 
-# if "win32" in sys.platform:
-#     asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-#     cmds = [["C:Windows/system32/HOSTNAME.EXE"]]
-# else:
-#     cmds = [
-#         ["du", "-sh", "/Users/fredrik/Desktop"],
-#         ["du", "-sh", "/Users/fredrik"],
-#         ["du", "-sh", "/Users/fredrik/Pictures"]
-#     ]
+load_dotenv()
 
-answers_prompt = ChatPromptTemplate.from_template("""
-    Using ONLY the following context answer the user's question.
-    If you can't just say you don't know, don't make anything up.
 
-    If you don't know the exact question, tell me something as close as possible that includes the words of the question I asked.
-                                                  
-    Then, give a score to the answer between 0 and 5.
-    The score should be high if the answer is related to the user's question, and low otherwise.
+def get_keyword_duck(inputs):
+    ddg = DuckDuckGoSearchResults()
+    keyword = inputs["keyword"]
+    return ddg.run(f"research of {keyword}")
 
-    If there is no relevant content, the score is 0.
-    Always provide scores with your answers
-    Context: {context}
 
-    Examples:
+def get_keyword_wiki(inputs):
+    wiki = WikipediaAPIWrapper()
+    keyword = inputs["keyword"]
+    return wiki.run(f"research of {keyword}")
 
-    Question: How far away is the moon?
-    Answer: The moon is 384,400 km away.
-    Score: 5
 
-    Question: How far away is the sun?
-    Answer: I don't know
-    Score: 0
+def research_overview(inputs):
+    url = inputs["url"]
+    headers = {
+        'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, "html.parser")
+        tags_to_search = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+        text_content = '\n'.join([element.get_text(strip=True)
+                                 for element in soup.find_all(tags_to_search)])
+        return text_content
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP Error: {e.response.status_code}")
+        return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
 
-    Your turn!
-    Question: {question}
-""")
+
+def save_to_file(inputs):
+    data = inputs["data"]
+    with open(f"search_result.txt", "w") as f:
+        f.write(data)
+    return f"Information saved in result.txt file."
+
+
+functions_map = {
+    "get_keyword_duck": get_keyword_duck,
+    "get_keyword_wiki": get_keyword_wiki,
+    "research_overview": research_overview,
+    "save_to_file": save_to_file,
+}
+
+functions = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_keyword_duck",
+            "description": "Show research results found via entered keyword and url.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "keyword": {
+                        "type": "string",
+                        "description": "The research of keyword",
+                    }
+                },
+                "required": ["keyword"],
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_keyword_wiki",
+            "description": "Show research results found via entered keyword and url.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "keyword": {
+                        "type": "string",
+                        "description": "The research of keyword",
+                    },
+                },
+                "required": ["keyword"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "research_overview",
+            "description": "Output highlights from a given search result via url.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "url": {
+                        "type": "string",
+                        "description": "The URL of websites for output highlights",
+                    },
+                },
+                "required": ["url"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "save_to_file",
+            "description": "Saves the given data to a file",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "data": {
+                        "type": "string",
+                        "description": "The data to save",
+                    }
+                },
+                "required": ["data"],
+            },
+        },
+    },
+]
+
+assistant_id = st.session_state.get("assistant_id", "")
+assistant_name = st.session_state.get("assistant_name", "")
+thread_id = st.session_state.get("thread_id", "")
+run_id = st.session_state.get("run_id", "")
+current_date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+
+def get_run(run_id, thread_id):
+    return client.beta.threads.runs.retrieve(
+        run_id=run_id,
+        thread_id=thread_id,
+    )
+
+
+def send_message(thread_id, content):
+    return client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=content
+    )
+
+
+def get_messages(thread_id):
+    messages = client.beta.threads.messages.list(thread_id=thread_id).data
+    messages.reverse()
+    for message in messages:
+        with st.chat_message(message.role):
+            if message.content:
+                st.markdown(message.content[0].text.value)
+                if hasattr(message.content[0], 'text'):
+                    if hasattr(message.content[0].text, 'annotations') and message.content[0].text.annotations:
+                        for annotation in message.content[0].text.annotations:
+                            st.markdown(f"Source: {annotation.ed}")
+
+
+def get_tool_outputs(run_id, thread_id):
+    run = get_run(run_id, thread_id)
+    outputs = []
+    for action in run.required_action.submit_tool_outputs.tool_calls:
+        action_id = action.id
+        function = action.function
+        print(
+            f"Calling function: {function.name} with arg {function.arguments}")
+        outputs.append(
+            {
+                "output": functions_map[function.name](json.loads(function.arguments)),
+                "tool_call_id": action_id,
+            }
+        )
+    return outputs
+
+
+def submit_tool_outputs(run_id, thread_id):
+    outputs = get_tool_outputs(run_id, thread_id)
+    return client.beta.threads.runs.submit_tool_outputs(
+        run_id=run_id,
+        thread_id=thread_id,
+        tool_outputs=outputs
+    )
 
 
 st.set_page_config(
-    page_title="SiteGPT",
-    page_icon="🖥️",
+    page_title="ResearchGPT",
+    page_icon="🕵️‍♂️",
 )
 
 
-st.title("SiteGPT")
 st.sidebar.markdown("""
 [💻Github repo &rarr;](https://github.com/bellukki/fullstack-gpt)  
-[📜Code of app &rarr;](https://github.com/bellukki/fullstack-gpt/blob/SiteGPT/app.py)
+[📜Code of app &rarr;](https://github.com/bellukki/fullstack-gpt/blob/researchGPT/app.py)
         """)
-
-url_cloudflare = "https://developers.cloudflare.com/sitemap.xml"
-url_openAI = "https://openai.com/sitemap.xml"
-
 api_key = st.sidebar.text_input(
     "Put your OpenAI API Key here", type="password")
-if api_key:
-    llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=0.1,
-        api_key=api_key,
-        callbacks=[StreamingStdOutCallbackHandler()],
-    )
+if api_key == "":
+    st.warning("Please enter your OpenAI API Key first!!")
+else:
+    client = client.Client(api_key=api_key)
+    if assistant_id == "":
+        my_assistants = client.beta.assistants.list(
+            order="desc",
+            limit="10",
+        )
+        for assistant in my_assistants.data:
+            if assistant.name == "Research AI Assistant":
+                st.write(f"Found assistant: {assistant.name}, {assistant.id}")
+                assistant_id = assistant.id
+                assistant_name = assistant.name
+                st.session_state["assistant_id"] = assistant_id
+                st.session_state["assistant_name"] = assistant_name
+                break
+
+        if assistant_id == "":
+            assistant = client.beta.assistants.create(
+                name="Research AI Assistant",
+                instructions="""
+                    You're an academic specializing in IT-related research materials.
+                    You should be well-versed in the latest issues and trends in IT and security. 
+                                                        
+                    Present as much academic research as possible for the given keywords, and be sure to provide URL sources and citations. 
+
+                    Without the "URL" it won't be found, so be sure to include it. 
+                                                    
+                    You must use all three tools provided (DuckDuckGo search tool, Wikipedia search tool, and ResearchOverview tool) and include both the search results and the extracted page, as well as the Wikipedia results.
+                                                        
+                    Be as detailed as possible in your answer, show it to us, and save it as a txt file when you're done. Be sure to provide URL sources and citations.
+                    """,
+                model="gpt-4-turbo-preview",
+                tools=functions,
+            )
+            st.session_state["assistant_id"] = assistant.id
+            st.session_state["assistant_name"] = assistant.name
+            assistant_id = assistant.id
+            assistant_name = assistant.name
+            st.write(f"Created a new assistant! ID: {assistant_id}")
+
     st.markdown(
         """
-        Ask questions about the content of a website.
-                
-        Start by writing the URL of the website on the sidebar.
+    # ResearchGPT
 
-        Or select the SITE what you want to find.
+    Welcome to ResearchGPT.
 
-        If you are forced to disconnect, please wait 3 seconds and select again.
-    """)
-
-else:
-    st.warning("Please enter your OpenAI API Key first!!")
-
-
-def get_answers(inputs):
-    docs = inputs['docs']
-    question = inputs['question']
-    answers_chain = answers_prompt | llm
-    return {
-        "question": question,
-        "answers":
-        [
-            {
-                "answer": answers_chain.invoke(
-                    {"question": question, "context": doc.page_content}
-                ).content,
-                "source": doc.metadata["source"],
-                "date": doc.metadata["lastmod"]
-            }
-            for doc in docs
-        ],
-    }
-
-
-choose_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-            Use ONLY the following pre-existing answers to answer the user's question.
-
-            Use the answers that have the highest score (more helpful) and favor the most recent ones.
-
-            Return the sources of the answers as they are, do not change them.
-
-            Answers: {answers}
-            """,
-        ),
-        ("human", "{question}"),
-    ]
-)
-
-
-def choose_answer(inputs):
-    answers = inputs["answers"]
-    question = inputs["question"]
-    choose_chain = choose_prompt | llm
-    condensed = "\n\n".join(
-        f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n"for answer in answers)
-    return choose_chain.invoke({
-        "question": question,
-        "answers": condensed
-    })
-
-
-def parse_page(soup):
-    header = soup.find("header")
-    footer = soup.find("footer")
-    if header:
-        header.decompose()
-    if footer:
-        footer.decompose()
-    return (
-        str(soup.get_text())
-        .replace("\n", " ")
-        .replace("\xa0", " ")
-        .replace("CloseSearch Submit Blog", "")
+    Enter keywords for the research results you want to find.
+    """
     )
 
+    if run_id != "":
+        run = get_run(run_id, thread_id)
+        if run.status == "completed":
+            st.success("The search is completed. The details are as follows")
+            get_messages(thread_id)
+            with open("search_result.txt", "rb") as file:
+                btn = st.download_button(
+                    label="Download result.txt",
+                    data=file,
+                    file_name=f"{current_date}_result.txt",
+                    mime="text/plain",
+                    on_click=lambda: setattr(
+                        st.session_state, "button_clicked", True),
+                )
+        elif run.status == "in_progress":
+            with st.status("In progress..."):
+                st.write("Waiting for the AI to respond...")
+                time.sleep(3)
+                st.rerun()
+        elif run.status == "requires_action":
+            with st.status("Processing action..."):
+                submit_tool_outputs(run_id, thread_id)
+                time.sleep(3)
+                st.rerun()
 
-@st.cache_resource(show_spinner="Loading Website...")
-def load_website(url, api_key):
-    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=1000,
-        chunk_overlap=200
-    )
-    if url == url_cloudflare or url == "https://developers.cloudflare.com/sitemap.xml":
-        loader = SitemapLoader(
-            url,
-            filter_urls=[
-                r"^(.*\/(workers-ai|vectorize|ai-gateway)\/).*",
-            ],
-            parsing_function=parse_page
-        )
-    else:
-        loader = SitemapLoader(
-            url,
-            parsing_function=parse_page
-        )
-    loader.requests_per_second = 2
-    docs = loader.load_and_split(text_splitter=splitter)
-    path_url = hashlib.md5(url.encode('utf-8')).hexdigest()
-    cache_path = f"./.cache/embeddings/site/{path_url}"
-    os.makedirs(cache_path, exist_ok=True)
-    cache_dir = LocalFileStore(cache_path)
-    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
-        OpenAIEmbeddings(api_key=api_key), cache_dir
-    )
-    vector_store = FAISS.from_documents(docs, cached_embeddings)
-    return vector_store.as_retriever()
+    if "clear" in st.session_state and st.session_state["clear"]:
+        st.session_state["input"] = ""
+        st.session_state["clear"] = False
 
+    keyword = st.text_input("Enter a research subject.",
+                            value=st.session_state.get("input", ""),
+                            key="input",)
 
-with st.sidebar:
-    choice = st.selectbox(
-        "Choose what you want to find documentation.",
-        (
-            "Search for what you want",
-            "Cloudflare",
-            "OpenAI",
-        ),
-    )
-    if choice == "Cloudflare":
-        url = url_cloudflare
-    elif choice == "OpenAI":
-        url = url_openAI
-    else:
-        url = st.text_input(
-            "Write down a URL",
-            placeholder="https://example.com/sitemap.xml",
-        )
-
-if st.sidebar.button("Refresh cache"):
-    clear_cache()
-    st.experimental_rerun()
-
-if url:
-    if ".xml" not in url:
-        with st.sidebar:
-            st.error("Please write down a Sitemap URL")
-    else:
-        retriever = load_website(url, api_key)
-        if url == url_cloudflare or url == "https://developers.cloudflare.com/sitemap.xml":
-            query = st.text_input("""
-        Ask a question about the following products of Cloudflare:
-        AI Gateway, Cloudflare Vectorize, Workers AI
-                                  """)
-        else:
-            query = st.text_input(
-                "Ask a question to the OpenAI website.")
-        st.session_state.query = query
-        if query:
-            chain = (
-                {
-                    "docs": retriever,
-                    "question": RunnablePassthrough(),
-                }
-                | RunnableLambda(get_answers)
-                | RunnableLambda(choose_answer)
+    if keyword:
+        if thread_id == "":
+            thread = client.beta.threads.create(
+                messages=[{"role": "user", "content": keyword}]
             )
-            result = chain.invoke(query)
-            st.markdown(result.content.replace("$", "\$"))
+            thread_id = thread.id
+            st.session_state["thread_id"] = thread_id
+        else:
+            send_message(thread_id, keyword)
+
+        run = client.beta.threads.runs.create(
+            thread_id=thread_id,
+            assistant_id=assistant_id,
+        )
+        run_id = run.id
+        st.session_state["run_id"] = run_id
+        st.session_state["clear"] = True
+        st.session_state["keyword"] = keyword
+        st.rerun()
